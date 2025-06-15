6fe8efda3b77ba517356ed8549ae6a3a41129a3c
Basher113 <147272612+Basher113@users.noreply.github.com>
2025-06-16 05:49:49 +0800

    Create header component
    - Delete react default styling

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import "./header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Simple Resume Builder</h1>
+      <p>Build your professional resume in minutes!</p>
+    </header>
+  )
+}
+
+export default Header
